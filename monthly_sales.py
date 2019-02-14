6e7bf4c5110ed1f5d0f4cde83ed405cf87acc6b5
gcb62 <46456419+gcb62@users.noreply.github.com>
2019-02-14 17:16:27 -0500

    Update monthly_sales.py

--- monthly_sales.py
+++ monthly_sales.py
@@ -3,12 +3,35 @@
 
 
 import os
-import pandas  
-# import plotly 
-# from plotly import graph_objs
+import pandas as pd
+import matplotlib.pyplot as pyp
+import matplotlib.ticker as tic
 
-import plotly
-import plotly.graph_objs as go
+
+
+# now using #matplotlib
+
+# import plotly
+# import plotly.graph_objs as go
 
 # ...adapted from in class importing of #pandas and #plotly functions
 
+
+
+print("-----------------------")
+print("MONTH: March 2018")
+
+print("-----------------------")
+print("CRUNCHING THE DATA...")
+
+print("-----------------------")
+print("TOTAL MONTHLY SALES: $12,000.71")
+
+print("-----------------------")
+print("TOP SELLING PRODUCTS:")
+print("  1) Button-Down Shirt: $6,960.35")
+print("  2) Super Soft Hoodie: $1,875.00")
+print("  3) etc.")
+
+print("-----------------------")
+print("VISUALIZING THE DATA...")
